cf306d72455fec79e7b6e09d69aa235dd83868ba
Ahn YoungSeon <dudtjs0920@naver.com>
2021-11-26 20:22:16 +0900

    #3 homepage router 사용하도록 수정

--- main.js
+++ main.js
@@ -2,21 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-const home = require('./template/home.js');
+const indexRouter = require('./route/indexRouter.js')
+
 
 // parse application/x-www-form-urlencoded
 // 사용자가 요청할 때 마다 호출
 app.use(bodyParser.urlencoded({extended: false}));
-app.get('/', (req, res) => {
-	const body = home.HOME();
-	res.send(home.HTML(body));
-});
 
 // 정적 파일 (css, js) 경로 등록
 // public 아래에 정적 파일 정리
 app.use('/public', express.static( __dirname + '/public'));
 // app.use('/topic', topicRouter);
-// app.use('/', indexRouter);
+
+app.use('/', indexRouter);
 
 app.use(function(req, res, next) {	
     res.status(404).send('Sorry cant find that!');
