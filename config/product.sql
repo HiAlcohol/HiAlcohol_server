756b65a45937cdd56424ef6d981f389646840378
alreadynyeong <kjw87130@naver.com>
2021-11-29 15:22:02 +0900

    product  테이블 드라이진 추가

--- config/product.sql
+++ config/product.sql
@@ -88,3 +88,18 @@ VALUES (NULL, '럼', '골드 오브 모리셔스');
 
 INSERT INTO `product`
 VALUES(NULL, '럼', '엘 도라도');
+
+INSERT INTO `product`
+VALUES(NULL, '드라이진', '봄베이 사파이어');
+
+INSERT INTO `product`
+VALUES(NULL, '드라이진', '고든스');
+
+INSERT INTO `product`
+VALUES(NULL, '드라이진', '비피터');
+
+INSERT INTO `product`
+VALUES(NULL, '드라이진', '런던 드라이 진');
+
+INSERT INTO `product`
+VALUES(NULL, '드라이진', '라리오스');
