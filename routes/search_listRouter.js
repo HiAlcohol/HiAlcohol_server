b1548a33b1feb439828c856cf5aa6df431d24928
Ahn YoungSeon <dudtjs0920@naver.com>
2021-12-07 21:51:51 +0900

    #17 db connection 정보 분리

--- routes/search_listRouter.js
+++ routes/search_listRouter.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const search_list = require('../template/search_list.js');
-const mysql = require('mysql');
+// const mysql = require('mysql');
+const db = require('../config/db')
 
 var url = require('url');
 
-var db = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database:'hialcohol'
-});
-db.connect();
+// var db = mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'root',
+//     database:'hialcohol'
+// });
+// db.connect();
 
 router.get('/', function(request, response){
 	db.query(`select * from recipe`, function(err, result){
