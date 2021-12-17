4a64292fd33a7e26010cab23a3bbc78558cf4985
Ahn YoungSeon <dudtjs0920@naver.com>
2021-12-17 19:53:08 +0900

    #23 게시글 상세보기 페이지 링크 연결

--- routes/likeRouter.js
+++ routes/likeRouter.js
@@ -10,33 +10,40 @@ router.get('/', function(request, response) {
 	if (request.user === undefined) {
 		response.redirect('/');
 	}
-	db.query(`SELECT post.id, post.title, post.createdate, count(*) 'count' 
-		FROM (SELECT * FROM post WHERE post.userId=${request.user.id}) post, liked 
-		WHERE post.id = liked.postId  group by post.id`, 
-	function(err, result) {
-		console.log(result);
-		var list ='';
-		console.log(result[0].createdate)
-		for (var i = 0;i < result.length; i++) {
-			list += `
-			<a href='/likes?id=${result[i].id}'>
-				<div class="content">
-                    <div class="subject">
-                        <p>${result[i].title}</p>
-                        <div class="info"><span>${request.user.nickname}</span> | <span>${result[i].createdate}</span></div>
-                    </div>
-                    <div class="like">
-                        <button type="button" class="likebtn" id="img_btn"><img src="/public/img/heart_outline.png"></button>
-                        <div class="cnt">${result[i].count}</div>
-                    </div>
-				</div>
-			</a>
-			`
-		}
-		var body = template.HOME(list);
-		var html = template.HTML(body);
-		response.send(html);
-	})
+	if (request.query.id === undefined) {
+		db.query(`SELECT post.id, post.title, post.createdate, count(*) 'count' 
+			FROM (SELECT * FROM post WHERE post.userId=${request.user.id}) post, liked 
+			WHERE post.id = liked.postId  group by post.id`, 
+		function(err, result) {
+			console.log(result);
+			var list ='';
+			console.log(result[0].createdate)
+			for (var i = 0;i < result.length; i++) {
+				list += `
+				<a href='/board/view?id=${result[i].id}'>
+					<div class="content">
+						<div class="subject">
+							<p>${result[i].title}</p>
+							<div class="info"><span>${request.user.nickname}</span> | <span>${result[i].createdate}</span></div>
+						</div>
+						<div class="like">
+							<button type="button" class="likebtn" id="img_btn"><img src="/public/img/heart_outline.png"></button>
+							<div class="cnt">${result[i].count}</div>
+						</div>
+					</div>
+				</a>
+				`
+			}
+			var body = template.HOME(list);
+			var html = template.HTML(body);
+			response.send(html);
+		})
+	} else {
+		db.query(`SELECT * FROM post WHERE id=?`, [request.query.id], function(err, result) {
+
+		})
+	}
+	
 });
 
 module.exports = router;
