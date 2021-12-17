f3f75c314f0aa30291550ed977d820ccc99700fc
Ahn YoungSeon <dudtjs0920@naver.com>
2021-12-17 21:53:52 +0900

    test-user insert sql 추가

---
+++ config/test-user.sql
@@ -0,0 +1,11 @@
+INSERT INTO `user`
+VALUES (NULL, 1, '/public/img/loginIcon', 'testUser1');
+
+INSERT INTO `user`
+VALUES (NULL, 2, '/public/img/loginIcon', 'testUser2');
+
+INSERT INTO `user`
+VALUES (NULL, 3, '/public/img/loginIcon', 'testUser1');
+
+INSERT INTO `user`
+VALUES (NULL, 4, '/public/img/loginIcon', 'testUser2');
