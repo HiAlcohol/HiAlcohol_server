447d3d34be9ca1d89e2e5fbff14776e7b553a64a
parksewon <tpdnjs1825@naver.com>
2021-12-17 23:11:28 +0900

    ADD: #13 글쓰기 및 글 상세보기 기능 추가

--- routes/boardProcessRouter.js
+++ routes/boardProcessRouter.js
@@ -3,14 +3,8 @@ const express = require('express');
 const { fstat } = require('fs');
 const router = express.Router();
 const mysql = require('mysql');
+const db = require('../config/db.js');
 
-// var db = require('../config/db.js')
-const db = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'mysql1234',
-	database: 'hialcohol'
-});
 
 
 router.post('/', function(request, response) {
