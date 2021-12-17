3b6f846c0c11dfc0e9fe6c202748234787596d14
alreadynyeong <kjw87130@naver.com>
2021-12-17 18:44:49 +0900

    #10 search list 검색 기능 수정

--- routes/search_listRouter.js
+++ routes/search_listRouter.js
@@ -4,9 +4,6 @@ const search_list = require('../template/search_list.js');
 var db = require('../config/db'); // db.js 폴더 경로
 var url = require('url');
 
-sql = `select recipe.cocktail, material.material from recipe, material  where
-material.id =any(select inclusion.materialId from inclusion where inclusion.recipeId= recipe.id )`
-
 class Item
 {
 	cocktail = ''
@@ -14,17 +11,18 @@ class Item
 }
 
 router.get('/', async function(request, response){
+	var _url = request.url;
+	var queryData = url.parse(_url, true).query;
+	
+	sql2 = `select recipe.cocktail, material.material from recipe, material  where recipe.id=any(select recipeId from inclusion 
+	where  materialId = any(select id from material 
+		where material= '${queryData.keyword}' or material = any(select alcolType from product where name='${queryData.keyword}'))) 
+		and material.id =any(select inclusion.materialId from inclusion where inclusion.recipeId= recipe.id )`
 
-	db.query(sql, function(err, result){
-		var _url = request.url;
-		var queryData = url.parse(_url, true).query;
-		console.log('sql1:',result);
-		sql2 = `select recipe.cocktail, material.material from recipe, material  where recipe.id=any(select recipeId from inclusion 
-			where  materialId = any(select id from material 
-				where material= '${queryData.keyword}' or material = any(select alcolType from product where name='${queryData.keyword}'))) 
-				and material.id =any(select inclusion.materialId from inclusion where inclusion.recipeId= recipe.id )`
-		
-
+	db.query(sql2, function(err, result){
+	
+		// console.log('sql1:',result);
+	
 		if (err) throw err;
 		var recipe_list = [];
 		var index = 0;
@@ -43,7 +41,7 @@ router.get('/', async function(request, response){
 				}
 			}
 		}
-		console.log(recipe_list)
+		// console.log(recipe_list)
 		var list = search_list.LIST(recipe_list)
 		var html = search_list.HTML( list)
 					
