fea134ed7e23735de5048c7375ecaa08de899c8e
alreadynyeong <kjw87130@naver.com>
2021-12-17 11:39:29 +0900

    #19 현재 위치 주변 주류유통 매장 표시하기

--- template/map.js
+++ template/map.js
@@ -1,15 +1,15 @@
 module.exports = {
-	HTML: function(body) {
-		const head = this.HEAD();
-		const tail = this.TAIL();
-		return `
-		${head}
-		${body}
-		${tail}
-		`
-	},
-	HOME: function() {
-		return `
+   HTML: function(body) {
+      const head = this.HEAD();
+      const tail = this.TAIL();
+      return `
+      ${head}
+      ${body}
+      ${tail}
+      `
+   },
+   HOME: function() {
+      return `
         <header>
         <!-- hamburger menu -->
         <div class="menu_btn">
@@ -62,74 +62,109 @@ module.exports = {
         <main>
         <!-- 우리동네 주류매장 -->
         <div id="map"></div>
+
         <script>
         var infowindow = new kakao.maps.InfoWindow({zIndex:1});
-        var mapContainer = document.getElementById('map'), 
+ 
+        if (navigator.geolocation) {
+         
+        navigator.geolocation.getCurrentPosition(function(position) {
+         
+            var geocoder = new kakao.maps.services.Geocoder();
+         
+            var lat = position.coords.latitude,
+            lon = position.coords.longitude; 
+         
+            var coord = new kakao.maps.LatLng(lat, lon);
+            var callback = function(result, status) {
+                if (status === kakao.maps.services.Status.OK) {
+                    console.log(result[0].address.address_name);
+                }
+            };
+         
+            geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
+       
+            var locPosition = new kakao.maps.LatLng(lat, lon), 
+            mapContainer = document.getElementById('map'), 
             mapOption = {
-                center: new kakao.maps.LatLng(37.566826, 126.9786567), 
-                level: 3 
-            };  
-        
-        var map = new kakao.maps.Map(mapContainer, mapOption); 
-        var ps = new kakao.maps.services.Places(); 
-        ps.keywordSearch('주류유통', placesSearchCB); 
-        
-        function placesSearchCB (data, status, pagination) {
-            if (status === kakao.maps.services.Status.OK) {
-        
-                var bounds = new kakao.maps.LatLngBounds();
-        
-                for (var i=0; i<data.length; i++) {
-                    displayMarker(data[i]);    
-                    bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-                }       
-        
-                map.setBounds(bounds);
-            } 
-        }
+                center: locPosition,
+                level: 5 
+            };
+         
+            var map = new kakao.maps.Map(mapContainer, mapOption);
+            var center = map.getCenter();
+    
+            // new kakao.maps.InfoWindow({ position: center, map: map, content: '현재 위치'});
+            var ps = new kakao.maps.services.Places(); 
+            
+            ps.keywordSearch('주류유통', placesSearchCB, {
+                location: center, 
+                sort: kakao.maps.services.SortBy.DISTANCE
+            }); 
+         
+            function placesSearchCB (data, status, pagination) {
+                if (status === kakao.maps.services.Status.OK) {
+                    for (var i=0; i<data.length; i++) {
+                        displayMarker(data[i]);
+                    }
+                }
+            }
         
         function displayMarker(place) {
-
-            var marker = new kakao.maps.Marker({
-                map: map,
-                position: new kakao.maps.LatLng(place.y, place.x) 
-            });
         
-            kakao.maps.event.addListener(marker, 'click', function() {
-                infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
-                infowindow.open(map, marker);
+            var placeText = place.place_name;
+         
+            if (placeText.indexOf("주류유통") != -1) {
+                var marker = new kakao.maps.Marker({
+                    map: map,
+                    position: new kakao.maps.LatLng(place.y, place.x),
+                })
+            } else {
+                var marker = new kakao.maps.Marker({
+                    map: map,
+                    position: new kakao.maps.LatLng(place.y, place.x),
+                });
+            }
+  
+        kakao.maps.event.addListener(marker, 'click', function() {
+         
+            infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+            infowindow.open(map, marker);
+            console.log(place.place_name);
             });
+        }
+         
+        });
         }
         </script>
         </main>
-	`
-	},
-	HEAD: function() {
-		return `
-		<!DOCTYPE html>
+   `
+   },
+   HEAD: function() {
+      return `
+      <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8" />
-	
-    
+   <meta charset="UTF-8" />
+   
     <link rel="stylesheet" href="public/css/map.css" />
     <link rel="stylesheet" href="public/css/menu.css" />
     <script src="public/js/menu.js" type="text/javascript"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-	<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=3a0965c75cdc0b99976416f11247b105&libraries=services"></script>
-	<script>
+   <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=3a0965c75cdc0b99976416f11247b105&libraries=services"></script>
+   <script>
             
     </script>
     <title>Hi Alcohol Map</title>
 
   </head>
   <body>
-		`;
-	},
-	TAIL: function() {
-		return `
-		</body>
+      `;
+   },
+   TAIL: function() {
+      return `
+      </body>
 </html>
-		`
-	}
+      `
+   }
 };
