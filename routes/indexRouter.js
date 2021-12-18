4cd1d3262d6b733729e220a99eb38e876d2cdcd1
alreadynyeong <kjw87130@naver.com>
2021-12-18 18:14:47 +0900

    #32 랜덤으로 레시피 불러오기, 해당 상세보기 연결

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,9 +4,15 @@ const router = express.Router();
 const home = require('../template/home.js');
 
 router.get('/', function(request, response) {
-	console.log('path: /');
-	const body = home.HOME(request.user);
-	response.send(home.HTML(body));
+	// console.log('path: /');
+	sql = `select * from recipe order by rand() limit 1`
+	db.query(sql, function(err, result){
+		if(err) throw err;
+
+		const body = home.HOME(request.user, result);
+		response.send(home.HTML(body));
+	})
+
 });
 
 module.exports = router;
