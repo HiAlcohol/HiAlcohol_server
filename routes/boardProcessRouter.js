5978ac6a5cfd0f525f9e2783fea11633b9e5e74d
parksewon <tpdnjs1825@naver.com>
2021-12-19 05:17:11 +0900

    #13 로그인 상태에서만 글작성 페이지로 넘어갈 수 있게 수정

--- routes/boardProcessRouter.js
+++ routes/boardProcessRouter.js
@@ -1,5 +1,6 @@
 const { request } = require('express');
 const express = require('express');
+const res = require('express/lib/response');
 const { fstat } = require('fs');
 const router = express.Router();
 const mysql = require('mysql');
@@ -8,17 +9,19 @@ const db = require('../config/db.js');
 
 
 router.post('/', function(request, response) {
-    const body = request.body;
-    let title = body.title;
-    let content = body.content;
-    var today = new Date()
-    var userID = request.user.kakaoid;
 
-
-    db.query(`INSERT INTO post (id, userID, title, content, createdate, updatedate) VALUES (?,?,?,?,now(),now())`, [null, userID, title, content, null, null], function(err, result){
-        if (err) console.error("err : " + err);
-        response.redirect('/board');
-    })
+        const body = request.body;
+        let title = body.title;
+        let content = body.content;
+        var today = new Date()
+        var userID = request.user.kakaoid;
+    
+    
+        db.query(`INSERT INTO post (id, userID, title, content, createdate, updatedate) VALUES (?,?,?,?,now(),now())`, [null, userID, title, content, null, null], function(err, result){
+            if (err) console.error("err : " + err);
+            response.redirect('/board');
+        })
+    
     
 });
 
--- routes/boardRouter.js
+++ routes/boardRouter.js
@@ -7,6 +7,7 @@ const board_view = require('../template/board_view.js');
 const mysql = require('mysql');
 const { post } = require('./indexRouter.js');
 const db = require('../config/db.js');
+const res = require('express/lib/response');
 
 function dateFormat(date) {
 	var newdate = new Date(date);
@@ -19,9 +20,19 @@ function dateFormat(date) {
 	return newdate.getFullYear() + '.' + month + '.' + day + ' ';
 }; 
 
+// router.get('/write', function(request, response) {
+// 		const body = board_write.HTML();
+// 		response.send(board_write.HTML(body));
+// });
+
 router.get('/write', function(request, response) {
-	const body = board_write.HTML();
-	response.send(board_write.HTML(body));
+	if(!request.isAuthenticated()){
+		response.send('<script>alert("로그인이 필요한 서비스입니다.");\
+		location.href="/oauth/kakao";</script>');
+	}else{
+		const body = board_write.HTML();
+		response.send(board_write.HTML(body));
+	}
 });
 
 router.get('/', function(request, response) {
@@ -40,9 +51,6 @@ router.get('/', function(request, response) {
 	});
 });
 
-router.get('/write', function(request, response) {
-	response.send(board_write.HTML(board_write.HTML));
-});
 
 router.get('/view', function(request, response){
 
@@ -72,21 +80,6 @@ router.get('/view', function(request, response){
 	});
 
 
-});
-
-router.post('/write', function(request, response) {
-    const body = request.body;
-    let title = body.title;
-    let content = body.content;
-    var today = new Date()
-
-
-    db.query(`INSERT INTO post (id, userID, title, content, createdate, updatedate) VALUES (?,?,?,?,now(),now())`, [null, '111', title, content, null, null], function(err, result){
-        if (err) console.error("err : " + err);
-        response.redirect('/board');
-    })
-
-    
 });
 
 module.exports = router; 
