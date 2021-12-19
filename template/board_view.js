980999de21a8ea4885cbcc3791b6f30834019d89
parksewon <tpdnjs1825@naver.com>
2021-12-20 02:24:09 +0900

    글 삭제 버튼 css 수정

--- template/board_view.js
+++ template/board_view.js
@@ -89,8 +89,8 @@ module.exports = {
                 <a href="/board/edit?id=${id}">수정</a>
                 <p>  |  </p>
                 <form action="/board_delete_process?id=${id}" method="post" >
-                <div class="delete">
-                    <input type="submit" value="삭제">
+                <div class="del">
+                    <input type="submit" id="del" name="del" value="삭제">
                 </div>
                 </form>
 				<div></div>
