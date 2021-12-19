4a9958b7d2e93cf9fb815ca7ec2809ffb7b9862a
kong <1018dbrud@gmail.com>
2021-12-19 18:59:47 +0900

    #7: board css 수정

--- template/board.js
+++ template/board.js
@@ -22,8 +22,9 @@ module.exports = {
         return `
         <div class="content">
             <div class="subject">
-                <p>${title}</p>
+                <p>${title}
                 <div class="info"><span>${userId}  </span> | <span>  ${createdate}</span></div>
+                </p>
             </div>
             <div class="like">
             <button type="button" class="likebtn" id="img_btn" onclick="didTapButton();"><img src="/public/img/heart_outline.png"></button>
@@ -40,126 +41,76 @@ module.exports = {
     },
     HEAD: function() {
         return `
-        <!DOCTYPE html>
-        <html lang="en">
         <head>
-            <meta charset="UTF-8">
-
-            
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-            <style>
-                /*모든 요소 적용*/
-                @import url('https://fonts.googleapis.com/css2?family=Pattaya&display=swap');
-                p{
-                font-family: 'Pattaya', sans-serif;
-                color: white;
-                }
-                @font-face {
-                font-family: 'GmarketSansMedium';
-                src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
-                font-weight: normal;
-                font-style: normal;
-                }   
-                *:not(i){
-                font-family:GmarketSansMedium;
-                }
-                
-                * {
-                    color: white;
-                }
-                body {
-                    width: 100%;
-                    height: 100%;
-                    padding: 0;
-                    margin: 0;
-                    background-color: #242424;
-                }
-                /*개별 적용*/
-                .wrapper {
-                    width: 500px;
-                    height: 100%;
-                    min-height: 100vh;
-                    background-color: #242424;
-                    margin: auto;
-                }
-                .menuBar {
-                    display: flex;
-                    justify-content: space-between;
-                    align-items: center;
-        
-                    width: 100%;
-                    height: 80px;
-
-        
-                    padding: 20px;
-                    box-sizing: border-box;
-        
-                    position: absolute;
-                    position: sticky;
-                    top: 0;
-                    
-                    background-color: #242424;
-                }
-
-                .logo {
-                    font-family: 'Pattaya';
-                    font-size: 25px;
-                    color: #0BF3BC;
-                }
-                
-                .writeIcon {
-                    width: 30px;
-                    height: 30px;
-                    background-color: teal;
-                }
-        
-                /*content*/
-                .contentList{
-                    padding: 20px;
-                    box-sizing: border-box;
-                }
-                .content {
-                    display: flex;
-                    justify-content: space-between;
-                    align-items: center;
-                    margin-bottom: 30px;
-                }
-                .content p {
-                    margin: 0;
-                }
-                .subject {
-                    font-size: 16px;
-                }
-                
-                .info, .info span {
-                    color: gray;
-                    font-size: 13px;
-                    margin-top: 10px;
-                }
-
-                .like {
-                    algin-items: center;
-                }
-                
-                .likebtn {
-                    background-color: #242424;
-                    border: none;
-                    align-items: center;
-                }
-
-            </style>
+        <link rel="stylesheet" href="public/css/menu.css" />
+        <link rel="stylesheet" href="public/css/home.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+        <script src="public/js/menu.js" type="text/javascript"></script>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+        crossorigin="anonymous"
+        />
+        <title>Hi Alcohol</title>
+        <link rel="stylesheet" href="public/css/board.css" />
         </head>
-        <body>
 
+        <style>
+            /*모든 요소 적용*/
+            @import url('https://fonts.googleapis.com/css2?family=Pattaya&display=swap');
+            p{
+            font-family: 'Pattaya', sans-serif;
+            color: white;
+            }
+            @font-face {
+            font-family: 'GmarketSansMedium';
+            src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
+            font-weight: normal;
+            font-style: normal;
+            }   
+            *:not(i){
+            font-family:GmarketSansMedium;
+            }
+        </style>
+
+        <body>
         <div class="wrapper">
         <div class="contentWrapper">
-        <!-- 메뉴바 -->
-        <div class="menuBar">
-            <div class="menuIcon"></div>
-            <div class="logo">Hi Alcohol</div>
-            <div class="writeIcon"></div>
+            <!-- 메뉴바 -->
+            <div class="header">
+				<div class="menu_btn">
+					<a href="#">
+					<div class="container">
+						<div class="bar1"></div>
+						<div class="bar2"></div>
+						<div class="bar3"></div>
+					</div>
+					</a>
+				</div>
+				<div class="menu_bg"></div>
+				<div class="sidebar_menu">
+					<div class="close_btn">
+						<a href="#">
+							<div class="container">
+								<img src="/public/img/back.png" height="18px" style="text-align: right; display: flexbox;"/>
+							</div>
+						</a>
+					</div>
+					<div class="menu_wrap">
+						<div><a href="/board">꿀조합 게시판</a></div>
+						<div><a href="/map">우리동네 주류매장</a></div>
+						<div><a href="/myboard">내가 쓴 꿀조합</a></div>
+						<div><a href="/likes">좋아요 리스트</a></div>
+						<div><a href="/logout">로그아웃</a></div>
+						// login 시에만 보이게 할 예정
+					</div>
+				</div>
+                <a href="/"><div class="logo">Hi Alcohol</div></a>
+				<div class = "writeBtn">
+					<a href="/board/write"><img class="writeBtn" src="/public/img/writeButton.png"></a>
+				</div>
             </div>
             <div class="contentList">
         `;
