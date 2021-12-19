abdc1b99043daf35701d3b8c9de5d99009faadf0
alreadynyeong <kjw87130@naver.com>
2021-12-19 10:21:56 +0900

    #31 search_list, recipe 메뉴 아이콘 동작 수정

--- template/recipe.js
+++ template/recipe.js
@@ -1,6 +1,8 @@
+const menu = require('./menu.js');
+
 module.exports = {
-	HTML: function(name, rate, content) {
-		
+	HTML: function(name, rate, content, user) {
+		var menu_list = menu.MENU(user);
 		return `
 
         <!DOCTYPE html>
@@ -39,46 +41,30 @@ module.exports = {
 <div class="board_wrap">
         <div class="main_title">
             <!-- hamburger menu -->
-    <div class="menu_btn">
-        <a href="#">
-            <div class="container">
-                <div class="bar1"></div>
-                <div class="bar2"></div>
-                <div class="bar3"></div>
-                <!-- <img src="menuIcon.png" width="40px" /> -->
-            </div>
-        </a>
+            <div class="menu_btn">
+            <a href="#">
+                <div class="container">
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3"></div>
+                </div>
+            </a>
+        </div>
+        
+        <a href="/" style="font-family: 'Pattaya', sans-serif; color: #0bf3bc">Hi Alcohol</a>
+        <div class="blank"></div>
     </div>
+    </header>
     <div class="menu_bg"></div>
     <div class="sidebar_menu">
         <div class="close_btn">
             <a href="#">
                 <div class="container">
-                    <div
-                            class="change bar1 a"
-                            style="
-                -webkit-transform: rotate(-45deg) translate(-1px, 1px);
-                transform: rotate(-45deg) translate(-1px, 1px);
-              "
-                    ></div>
-                    <div
-                            class="bar3 change b"
-                            style="
-                -webkit-transform: rotate(45deg) translate(-4px, -4px);
-                transform: rotate(45deg) translate(-4px, -4px);
-              "
-                    ></div>
+                    <img src="/public/img/back.png" height="18px" style="text-align: right; display: flexbox;"/>
                 </div>
-                <!-- <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMjMuOTU0IDIxLjAzbC05LjE4NC05LjA5NSA5LjA5Mi05LjE3NC0yLjgzMi0yLjgwNy05LjA5IDkuMTc5LTkuMTc2LTkuMDg4LTIuODEgMi44MSA5LjE4NiA5LjEwNS05LjA5NSA5LjE4NCAyLjgxIDIuODEgOS4xMTItOS4xOTIgOS4xOCA5LjF6Ii8+PC9zdmc+"> -->
             </a>
         </div>
-        <div class="menu_wrap">
-            <div><a href="#">꿀조합 게시판</a></div>
-            <div><a href="#">우리동네 주류매장</a></div>
-            <div><a href="#">내가 쓴 꿀조합</a></div>
-            <div><a href="#">좋아요 리스트</a></div>
-            <div><a href="#">로그아웃</a></div>
-        </div>
+        ${menu_list}
     </div>
         <div class="hi_alcohol">
             <a href="/" style="font-family: 'Pattaya', sans-serif; color: #0bf3bc">Hi Alcohol</a>
