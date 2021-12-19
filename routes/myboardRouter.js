30afa0592f14d74c6daf19e1b5d9d286db9c01f9
Ahn YoungSeon <dudtjs0920@naver.com>
2021-12-19 19:48:11 +0900

    #43 myboard list query 수정

--- routes/myboardRouter.js
+++ routes/myboardRouter.js
@@ -9,27 +9,27 @@ router.get('/', function(request, response) {
 	if (request.user === undefined) {
 		response.redirect('/');
 	}
-	db.query(`SELECT post.id, post.title, post.createdate, count(*) 'count' 
-	FROM (SELECT * FROM post WHERE post.userId=${request.user.id}) post, liked 
-	WHERE post.id = liked.postId group by post.id`, function(err, result) {
+	db.query(`SELECT post.id, post.title, post.createdate, count(liked.id) 'count' 
+	FROM (SELECT * FROM post WHERE post.userId=${request.user.id}) post left join liked 
+	on post.id = liked.postId group by post.id`, function(err, result) {
 		if (err) throw err;
 		console.log(result)
 		var list ='';
 		console.log(result[0].createdate)
 		for (var i = 0;i < result.length; i++) {
 			list += `
-			<a href='/board/view?id=${result[i].id}'>
-				<div class="content">
+			<div class="content">
+				<a href='/board/view?id=${result[i].id}'>
 					<div class="subject">
 						<p>${result[i].title}</p>
 						<div class="info"><span>${request.user.nickname}</span> | <span>${result[i].createdate}</span></div>
 					</div>
+				</a>
 					<div class="like">
 						<button type="button" class="likebtn" id="img_btn"><img src="/public/img/heart_outline.png"></button>
 						<div class="cnt">${result[i].count}</div>
 					</div>
-				</div>
-			</a>
+			</div>
 			`
 		}
 		const body = boardlist.HOME(list, request.user);
