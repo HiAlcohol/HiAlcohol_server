c30e2d97399f3a96001df78384ff34d23ff2dd57
Ahn YoungSeon <dudtjs0920@naver.com>
2022-01-19 16:29:27 +0900

    HALC-48 댓글 삭제 api 추가

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -4,6 +4,7 @@ const db = require('../config/db.js');
 
 // prefix: /comment
 
+// { postId: , comment: }
 router.post('/', function(req, res) {
 	const body = req.body;
 	console.log(req.body);
@@ -17,6 +18,41 @@ router.post('/', function(req, res) {
 				throw err
 			console.log(result);
 			res.status(201).json('댓글 추가 성공');
+		})
+	}
+});
+
+// {userId: , postId: , commentId: }
+router.post('/del', async function(req, res) {
+	const body = req.body;
+
+	if (!req.isAuthenticated()) {
+		res.status(401).send({error: '로그인이 필요한 서비스입니다.'});
+	} else if (parseInt(body.userId) !== req.user.id) {
+		res.status(403).send({error: "자신이 작성한 댓글만 삭제할 수 있습니다."});
+	}
+
+	let promise = new Promise((resolve, reject) => {
+		db.query("SELECT * FROM comment WHERE id = ?", [parseInt(body.commentId)],
+		function(err, result) {
+			if (err)
+				throw err
+			if (result[0].userId == body.userId &&
+				result[0].postId == body.postId)
+				resolve(result[0].id)
+			else
+				resolve(0)
+		})
+	});
+	let commentId = await promise;
+
+	if (commentId === 0)
+		res.status(403).json('잘못된 호출입니다.');
+	else {
+		db.query("DELETE FROM comment WHERE id=?", [parseInt(body.commentId)],
+		function(err, result) {
+			console.log(result);
+			res.status(200).send('댓글 삭제 성공');
 		})
 	}
 });
