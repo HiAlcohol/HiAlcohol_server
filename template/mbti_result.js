b43f813d54bc72fb0fd7ac93f5b1b512b79d758e
alreadynyeong <kjw87130@naver.com>
2022-01-24 18:02:35 +0900

    #54 하이 알코올 페이지 연결

--- template/mbti_result.js
+++ template/mbti_result.js
@@ -10,27 +10,6 @@ module.exports = {
     },
     HOME: function(data, des) {
        return `
-       <script>
-
-      // 카카오 공유를 위한 임시코드
-    Kakao.init('b305a1173710e4ff765e09b5a62e314c'); 
-    function link_kakao() {
-      var sh_title = "나와 어울리는 칵테일은?";
-      var sh_desc = "당신에게 어울리는 칵테일을 찾아보세요.\nHi Alcohol"; 
-      Kakao.Link.sendDefault({ 
-        objectType: 'feed', 
-        content: {
-         title: sh_title, description: sh_desc, 
-         imageUrl: "../img/cocktail.png", 
-         link: { mobileWebUrl: "모바일 링크 url", 
-         webUrl: "PC 링크 url" 
-        } 
-      }, 
-    }); 
-    }
-   
-   </script>
-   
        <body>
          <div class="result">
            <a>당신에게 어울리는 술은</a><br>
@@ -58,9 +37,15 @@ module.exports = {
            <div class="button"><button id="restart" class="btn" onClick="location.href='../mbti' "> 처음으로</button></div><br>
            
            <div class="copy"><button id="copy" class="btn">링크 복사</button></div>
-           <div class="share"><img src="/public/img/bt_kakao.png"><br>공유하기</div>
+           <div class="share"><button id = "share" class="btn"><img src="/public/img/bt_kakao.png"><br>공유하기</button></div>
            
            <input type="text" value="복사될 URL 경로" id="link" style="display:none;">
+
+           <div class="site">
+           <span style= "color:white;">더 많은 술을 추천받고싶다면?<br>술 조합 추천 웹사이트</span>
+           
+           <a href="/" style="font-family: 'Pattaya', sans-serif; font-size:30px;color: #0bf3bc">Hi Alcohol</a>
+           </div>
          </main>
 
      
@@ -77,6 +62,9 @@ module.exports = {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         <script src="/public/js/share.js" type="text/javascript"></script>
+        <script>
+        Kakao.init('b305a1173710e4ff765e09b5a62e314c'); 
+        </script>
        
        </head>
    
