61c334cd46422a2b2fd43347c2404f5ed4357cc1
alreadynyeong <kjw87130@naver.com>
2022-01-25 17:25:04 +0900

    답변 선택시 질문 넘어가기

--- routes/mbtiRouter.js
+++ routes/mbtiRouter.js
@@ -58,7 +58,6 @@ router.get('/test', function(request, response){
 		httpOnly: true,
 		signed: true
 	})
-    num = 1;
     const qna = mbti_test.QNA(num, q, a1, a2);
     const body = mbti_test.HOME(qna);
 	response.send(mbti_test.HTML(body));
