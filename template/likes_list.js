660ce0f10b979d11de1f4c453577ff88653d9816
Ahn YoungSeon <dudtjs0920@naver.com>
2022-02-11 18:14:44 +0900

    내가쓴 꿀조합, 좋아요 리스트 title 수정

--- template/likes_list.js
+++ template/likes_list.js
@@ -57,7 +57,7 @@ module.exports = {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Hi Alcohol</title>
 
     <link rel="stylesheet" href="/public/css/board.css">
 	<link rel="stylesheet" href="/public/css/menu.css" />
