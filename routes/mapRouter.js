f0cd4928c6e24279b5bd74f96d72bbf42fd62beb
alreadynyeong <kjw87130@naver.com>
2022-02-15 11:38:58 +0900

    map 현재위치에서 시작하도록 수정

--- routes/mapRouter.js
+++ routes/mapRouter.js
@@ -7,7 +7,10 @@ var db = require('../config/db'); // db.js 폴더 경로
 router.get('/', function(request, response){
   
     var location = request.query.location;
-    const body = map.HOME(request.user, location);
+    
+    const a = map.MAP(location);
+    // console.log(a);
+    const body = map.HOME(request.user, location, a);
 	response.send(map.HTML(body));
 });
 
