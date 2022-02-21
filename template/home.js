5625a1ad9ab19e93458db7f05dec9de17588f78c
Ahn YoungSeon <dudtjs0920@naver.com>
2022-02-21 20:39:41 +0900

    HALC-54 SEO 태그 추가

--- template/home.js
+++ template/home.js
@@ -32,6 +32,7 @@ module.exports = {
 		
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
 	<link
 	rel="stylesheet"
 	href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
@@ -148,8 +149,16 @@ module.exports = {
 <head>
 	<meta charset="UTF-8" />
 
-	<title>Hi Alcohol | 하이알코올</title> 
+	<title>Hi Alcohol | 하이알코올</title>
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="Hi Alcohol">
     <meta name= "description" content="내가 가진 재료로 만들 수 있는 칵테일은? 칵테일 레시피 검색 웹사이트 하이 알코올!">
+	<meta property="og:image" content="/public/img/cocktail.png">
+	<meta property="og:url" content="https://hialcohol.xyz">
+	<meta property="twitter:card" content="summary">
+	<meta property="twitter:title" content="Hi Alcohol">
+	<meta property="twitter:description" content="내가 가진 재료로 만들 수 있는 칵테일은? 칵테일 레시피 검색 웹사이트 하이 알코올!">
+	<meta property="twitter:image" content="/public/img/cocktail.png">
 	
 	<link rel="stylesheet" href="/public/css/menu.css" />
 	<link rel="stylesheet" href="/public/css/home.css" />
